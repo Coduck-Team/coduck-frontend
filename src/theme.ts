ea623954d79b3c8308d028b84916e437984354b7
Seunghyun Park <98291947+seunghyun-24@users.noreply.github.com>
2025-06-26 16:16:39 +0900

    change color

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import { mainModule } from 'process';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -9,6 +10,24 @@ const roboto = Roboto({
 });
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffcc33',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#fff1c2', // #fffde7
+      contrastText: '#000000',
+    },
+    info: {
+      main: '#fff7cc',
+      contrastText: '#000000',
+    },
+    background: {
+      default: '#ffffff',
+      paper: '#f9f9f9',
+    },
+  },
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
